bff46450b4a1ae76123a3e6ff077af86c494b00f
LukeSandersJet <lsanders@jet-airways.com>
2022-06-16 15:43:36 -0500

    connect flask to mongoDB

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, jsonify, request, session, Response
+from flask import Flask, render_template, jsonify, request, session, Response, redirect
 from flask_cors import CORS
 import requests
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
+
+client = MongoClient('localhost', 27017)
+db = client.new_DB
+collection = db.new_collection
 
 @app.route('/')
 def hello_world():
