150258fddce388cf907619afadd310529a53fd7b
MountainGator <luketaylorsanders@gmail.com>
2022-06-17 06:30:00 -0500

    add models and seed files

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 CORS(app)
 
 client = MongoClient('localhost', 27017)
-db = client.new_DB
-collection = db.new_collection
+db = client.cupcakes_db
+collection = db.cakes_list
 
 @app.route('/')
 def hello_world():
