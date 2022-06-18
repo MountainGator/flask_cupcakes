e230fe08d18a126572c2441a40072251ad6d60a7
MountainGator <luketaylorsanders@gmail.com>
2022-06-18 14:08:06 -0500

    create all db routes

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ collection.insertMany([c1,c2])
 @app.route('/cupcakes', methods="GET")
 def get_cupcakes():
     all_cakes = collection.find({})
-    return Response(all_cakes, 200)
+    return Response(jsonify(all_cakes), 200)
 
 @app.route('/cupcakes/<int:id>', methods="GET")
 def get_single_cake(id):
     cake = collection.find({'_id': id})
-    return Response(cake, 200)
+    return Response(jsonify(cake), 200)
 
 @app.route('/cupcakes', methods="POST")
 def create_cupcake():
@@ -29,7 +29,20 @@ def create_cupcake():
     collection.insertOne(new_cake)
 
     print('new cupcake:',collection.find({'_id': new_cake['_id']}))
-    return Response(new_cake, 200)
+    return Response(jsonify(new_cake), 201)
+
+@app.route('/cupcakes/update/<int:id>', methods="PATCH")
+def update_cake(id):
+    new_data = request.json
+    collection.updateOne({'_id': id}, new_data)
+    verify = collection.find({'_id': id})
+    return Response(jsonify(verify), 203)
+
+@app.route('cupcakes/delete/<int:id>')
+def delete_cake(id):
+    collection.deleteOne({'_id': id})
+    verify = collection.find({})
+    return Response(jsonify(verify), 204)
 
 
 app.run()
